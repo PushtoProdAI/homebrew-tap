e7f1b5dcf14c1ff5653dccae0f43447e0f099530
Nathan Stehr <nstehr@meroxa.io>
2025-12-03 14:01:16 -0500

    Update prod.rb
    Correcting checksums

--- Formula/prod.rb
+++ Formula/prod.rb
@@ -10,7 +10,7 @@ class Prod < Formula
     on_macos do
       if Hardware::CPU.arm?
         url "https://ithjwwsjhdziqnxrwydg.supabase.co/storage/v1/object/public/cli-binaries/releases/latest/prod-v0.0.2-darwin-arm64.gz"
-        sha256 "e52ec34aff81e52dee077893edee8aa5608592742c43084095dcbdf57120fa15"
+        sha256 "162098e0c3a158f247ef5d67fcc8c19f2021dafd1a016d49a0d66d000b31f61c"
 
         def install
           bin.install "prod-v0.0.2-darwin-arm64" => "prod"
@@ -18,7 +18,7 @@ class Prod < Formula
       end
       if Hardware::CPU.intel?
         url "https://ithjwwsjhdziqnxrwydg.supabase.co/storage/v1/object/public/cli-binaries/releases/latest/prod-v0.0.2-darwin-amd64.gz"
-        sha256 "b26237ae239bd1164a55448a643288764b9a2a7d056ccd074759e3c6c0bcefbd"
+        sha256 "553f3b6cc0a797ddd91b82da095fb6013843fdd1143f276195544efcf1e0b1eb"
 
         def install
           bin.install "prod-v0.0.2-darwin-amd64" => "prod"
@@ -29,7 +29,7 @@ class Prod < Formula
     on_linux do
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://ithjwwsjhdziqnxrwydg.supabase.co/storage/v1/object/public/cli-binaries/releases/latest/prod-v0.0.2-linux-arm64.gz"
-        sha256 "150e2b301005d97a5e21c943c457b4eee2b14a4baeb99b0a00d312c9e40daa40"
+        sha256 "fd0d87923bc2d2c23d7878db3612bbf71f0b619c2f590748d46d60914c73610d"
 
         def install
           bin.install "prod-v0.0.2-linux-arm64" => "prod"
@@ -37,7 +37,7 @@ class Prod < Formula
       end
       if Hardware::CPU.intel?
         url "https://ithjwwsjhdziqnxrwydg.supabase.co/storage/v1/object/public/cli-binaries/releases/latest/prod-v0.0.2-linux-amd64.gz"
-        sha256 "afc2932fa761e8d9ce7f524678cb04e63ea5c664a506f783ef39e2082502cc46"
+        sha256 "720db7e380035a6e10db0f4a3868c298611d9c4e4aee32ec2fafe1d6e060ed91"
 
         def install
           bin.install "prod-v0.0.2-linux-amd64" => "prod"
